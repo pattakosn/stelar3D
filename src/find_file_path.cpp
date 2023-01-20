22eb1f74663b8b802a15d6e7f79a3b08695ae28d
N. Pattakos <pattakosn@fastmail.com>
2023-01-20 09:44:47 +0100

    use configured assets_path header file to locate assets

--- src/find_file_path.cpp
+++ src/find_file_path.cpp
@@ -1,11 +1,11 @@
 #include "find_file_path.h"
 #include <filesystem>
 #include <iostream>
-//#include "assets_path.h"
+#include "assets_path.h"
 namespace fs = std::filesystem;
 
 std::string find_file_path( std::string name) {
-    for (auto const& dir_entry : fs::recursive_directory_iterator("../../../..")){ //PROJECT_TOP_LEVEL_PATH.cstr()))
+    for (auto const& dir_entry : fs::recursive_directory_iterator(PROJECT_DIR_PATH)){
        // std::cout << "checking: " << dir_entry.path()<<std::endl;
         if ( auto const fname = dir_entry.path().string(); fname.find(name) != std::string::npos)
             return fname.c_str();
